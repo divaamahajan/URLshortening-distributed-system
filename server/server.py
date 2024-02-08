ca77cb8ebc9854d6b57f8271b9d69bb7b2a9e109
divaamahajan <dm.divya.mahajan@gmail.com>
2024-02-07 21:27:40 -0800

    redirection

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 import random
 
 app = Flask(__name__)
@@ -8,12 +8,13 @@ MAX_LEN = 7
 def shorten_url():
     data = request.json
     long_url = data.get("url")
-    shortened_url = getshorturl()
-    # while shortened url in DB: shortened_url = getshorturl()
-    shortened_url = "https://"+get_current_host()+"/"+shortened_url
-    return jsonify({"shortenedUrl": shortened_url})
+    short_url = create_short_url()
+    # while shortened url in DB: short_url = create_short_url()
+    short_url = "http://"+get_current_host()+"/short/"+short_url
+    # store long_url, short_url to DB
+    return jsonify({"shortenedUrl": short_url})
 
-def getshorturl():
+def create_short_url():
     chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     length = random.choice(range(1,MAX_LEN+1))
     short_url = "".join(random.choice(chars) for _ in range(length))
@@ -28,10 +29,19 @@ def get_current_host():
     except Exception as e:
         print("Error:", e)
         return None
-    
+
+
+@app.route("/short/<short_url>", methods=["GET"])
+def get_short_url(short_url):
+    # Get long_url from short_url
+    long_url = get_long_url(short_url)
+    # Redirect to long_url
+    print("short_url", short_url, "long_url", long_url)
+    return redirect(long_url)
+
+def get_long_url(short_url):
+    # Placeholder logic to retrieve long URL from short URL from DB
+    return "https://youtube.com/"
 
 if __name__=="__main__":
-    max_s = 62**7 - 1
-    print("Maximum value of s:", max_s)
-
     app.run(debug=True)
