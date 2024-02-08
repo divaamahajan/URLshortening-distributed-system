394a6a60235c93d0039570c412c0ddefa5502af1
divaamahajan <dm.divya.mahajan@gmail.com>
2024-02-07 16:40:19 -0800

    frontEnd

--- client/src/ShortURL.js
+++ client/src/ShortURL.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+// import React, { useState, useEffect } from "react";
 
 export default function ShortURL({ shortenedURL }) {
   // useEffect(() => {
